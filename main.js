3e176fae1e7de9d0786b8a374e4dc8833d78efff
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-03-21 11:16:43 -0400

    update

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 let listItems1 = document.querySelectorAll("#firstList li");
-for (let i = 0; i < listItems1.length; i++) {
-  listItems1[i].addEventListener("click", function () {
-    listItems1[i].style.textDecoration = "line-through";
+listItems1.forEach(function (item) {
+  item.addEventListener("click", function () {
+    item.style.textDecoration = "line-through";
   });
-}
-
-let listItems2 = document.querySelectorAll("#secondList li");
-for (let i = 0; i < listItems2.length; i++) {
-  listItems2[i].addEventListener("click", function () {
-    listItems2[i].style.opacity = "0";
-  });
-}
-
-let imgItems = document.querySelectorAll("#row img");
-for (let i = 0; i < imgItems.length; i++) {
-  imgItems[i].addEventListener("click", function () {
-    imgItems[i].style.width = "0px";
-  });
-}
-let button = document.querySelector("#destroy-all");
-button.addEventListener("click", function(){
-    burn()
 });
 
 
+let listItems2 = document.querySelectorAll("#secondList li");
+listItems2.forEach(function (item){
+  item.addEventListener("click", function () {
+    item.style.opacity = "0";})
+})
+
+
+let imgItems = document.querySelectorAll("#row img");
+imgItems.forEach(function (item){
+  item.addEventListener("click", function () {
+    item.style.width = "0px"})
+})
+
+
+let button = document.querySelector("#destroy-all");
+button.addEventListener("click", function () {
+  burn();
+});
 
 function burn() {
-  for (let i = 0; i < listItems1.length; i++) {
-      listItems1[i].style.textDecoration = "line-through";
-    };
-  for (let i = 0; i < listItems2.length; i++) {
-      listItems2[i].style.opacity = "0";
-    };
-  for (let i = 0; i < imgItems.length; i++) {
-      imgItems[i].style.width = "0px";
-    };
-  }
+  listItems1.forEach(function (item){
+    item.style.textDecoration = "line-through"
+  })
+
+  listItems2.forEach(function (item) {
+    item.style.opacity = "0";
+  })
+
+  imgItems.forEach(function (item){
+    item.style.width = "0px";
+  })
+}
 
